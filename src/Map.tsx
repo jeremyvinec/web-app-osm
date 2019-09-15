7734f99050fcb4a205b8a230c42fef7d6e0e4a4c
jeremy yvinec <jeremy29410@gmail.com>
2019-09-15 19:48:37 +0200

    state viewport

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,124 +1,33 @@
-import * as React from "react";
-import {
-    ViewState,
-    InteractiveMap,
-    CanvasOverlay,
-    SVGOverlay,
-    HTMLOverlay,
-    FullscreenControl,
-    GeolocateControl,
-    CanvasRedrawOptions,
-    HTMLRedrawOptions,
-    SVGRedrawOptions,
-    StaticMap
-} from 'react-map-gl';
-import * as MapboxGL from "mapbox-gl";
+import React, { Component } from "react";
+import ReactMapGL from "react-map-gl";
 
-interface State {
-    viewState: ViewState;
-}
-
-export default class Map extends React.Component<{}, State> {
-
-    readonly state: State = {
-        viewState: {
-            bearing: 0,
-            latitude: 0,
-            longitude: 0,
-            zoom: 3,
+export default class Map extends Component{
+    state = {
+        viewport: {
+            latitude: 48.452388799999994,
+            longitude: -3.9558419999999996,
+            width: "100vw",
+            height: "100vh",
+            zoom: 10
         }
-    };
+    }
 
     render() {
+        const { viewport } = this.state
         return (
             <div>
-                <InteractiveMap
-                    {...this.state.viewState}
-                    mapboxApiAccessToken="pk.test"
-                    height={400}
-                    width={400}
-                    ref={this.setRefInteractive}
-                >
-                    <FullscreenControl className="test-class" container={document.querySelector('body')} />
-                    <GeolocateControl className="test-class" />
-                    <CanvasOverlay
-                        redraw={opts => {
-                            const {
-                                ctx,
-                                height,
-                                project,
-                                unproject,
-                                width,
-                            } = opts;
-                            const xy: number[] = unproject(project([20, 20]));
-                            ctx.clearRect(0, 0, width, height);
-                        }}
-                    />
-                    <CanvasOverlay
-                        redraw={opts => {}}
-                        captureScroll={true}
-                        captureDrag={true}
-                        captureClick={true}
-                        captureDoubleClick={true}
-                    />
-                    <SVGOverlay
-                        redraw={() => {}}
-                    />
-                    <SVGOverlay
-                        redraw={opts => {
-                            const {
-                                height,
-                                project,
-                                unproject,
-                                width,
-                            } = opts;
-                            const xy: number[] = unproject(project([20, 20]));
-                        }}
-                        captureScroll={true}
-                        captureDrag={true}
-                        captureClick={true}
-                        captureDoubleClick={true}
-                    />
-                    <HTMLOverlay
-                        redraw={() => {}}
-                    />
-                    <HTMLOverlay
-                        redraw={opts => {
-                            const {
-                                height,
-                                project,
-                                unproject,
-                                width,
-                            } = opts;
-                            const xy: number[] = unproject(project([20, 20]));
-                        }}
-                        style={{
-                            border: "2px solid black"
-                        }}
-                        captureScroll={true}
-                        captureDrag={true}
-                        captureClick={true}
-                        captureDoubleClick={true}
-                    />
-                </InteractiveMap>
-                <StaticMap
-                    {...this.state.viewState}
-                    mapboxApiAccessToken="pk.test"
-                    height={400}
-                    width={400}
-                    ref={this.setRefStatic}
+                <ReactMapGL
+                    {... this.state}
+                    mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+                    mapStyle="mapbox://styles/mapbox/outdoors-v11"
+                    width={viewport.width}
+                    height={viewport.height}
+                    latitude={viewport.latitude}
+                    longitude={viewport.longitude}
+                    zoom={viewport.zoom}
+                    onViewportChange={(viewport) => this.setState({viewport})}
                 />
             </div>
         );
-    }
-
-    private map: MapboxGL.Map;
-
-    private readonly setRefInteractive = (el: InteractiveMap) => {
-        this.map = el.getMap();
-    }
-
-    private readonly setRefStatic = (el: StaticMap) => {
-        this.map = el.getMap();
     }
 }
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export default interface viewState {
+    latitude: number,
+    longitude: number,
+    width: string,
+    height: string,
+    zoom: number
+}
